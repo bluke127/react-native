b1762d80fb630859eaaa0bc2acfb0216287c5b97
bluke127 <dnehdrkd123@naver.com>
2025-01-23 23:28:39 +0900

    [feat]touchable

--- App.js
+++ App.js
@@ -1,113 +1,49 @@
-import * as Location from "expo-location";
-import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
-  View,
-  Text,
-  Dimensions,
-  ActivityIndicator,
   StyleSheet,
-  ScrollView,
+  Text,
+  View,
+  // 클릭하면 투명되는 애니메이션 
+  TouchableOpacity,
+  //요소를 클릭할때 배경이 바뀜
+  TouchableHighlight,
+  //화면상 변경은 없지만 이벤트만 실행할때 사용
+  TouchableWithoutFeedback,
+  Pressable,
 } from "react-native";
-import {Ionicons} from '@expo/vector-icons'
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-const API_KEY = "05804166b416f5bf2b990e9b13783a62"
+import { theme } from "./colors";
 export default function App() {
-  const [city, setCity] = useState("Loading...");
-  const [days, setDays] = useState([]);
-  const [ok, setOk] = useState(true);
-  const  getWeather = async () => {
-    //권한요청
-    const { granted } = await Location.requestForegroundPermissionsAsync();
-    if (!granted) {
-      console.log(a)
-      setOk(false);
-    }
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
-    );
-    console.log(location)
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    );
-    const json = await response.json();
-    console.log(json.list,json,"dss")
-    setDays(a=>[...json.list]);
-  };
-  useEffect(() => {
-     getWeather();
-  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        {/* 클릭하면 투명되는 애니메이션 */}
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
       </View>
-      <ScrollView
-        //페이지네이션 가능여부
-        pagingEnabled
-        //가로세로 여부
-        horizontal
-        //폰 하단에 검은색 스크롤
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            {/* 로딩 */}
-            <ActivityIndicator
-              color="white"
-              style={{ marginTop: 10 }}
-              size="large"
-            />
-          </View>
-        ) : (
-          days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <Text>{index}</Text>
-              <Text style={styles.temp}>
-                {parseFloat(day.main.temp).toFixed(1)}
-              </Text>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-            </View>
-          ))
-        )}
-      </ScrollView>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
-  city: {
-    flex: 1.2,
-    justifyContent: "center",
-    alignItems: "center",
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
   },
-  cityName: {
-    fontSize: 58,
-    fontWeight: "500",
-  },
-  weather: {},
-  day: {
-    width: SCREEN_WIDTH,
-    alignItems: "center",
-  },
-  temp: {
-    marginTop: 50,
+  btnText: {
+    fontSize: 38,
     fontWeight: "600",
-    fontSize: 178,
-  },
-  description: {
-    marginTop: -30,
-    fontSize: 60,
-  },
-  tinyText: {
-    fontSize: 20,
+    color: "white",
   },
 });

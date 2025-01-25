b206ba08a98160dcad4483b2475a24e8f4660d91
bluke127 <dnehdrkd123@naver.com>
2025-01-25 16:04:40 +0900

    [feat]네이티브 연습

--- App.js
+++ App.js
@@ -1,30 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react"
 import {
   StyleSheet,
   Text,
   View,
-  // 클릭하면 투명되는 애니메이션 
+  // 클릭하면 투명되는 애니메이션
   TouchableOpacity,
   //요소를 클릭할때 배경이 바뀜
   TouchableHighlight,
   //화면상 변경은 없지만 이벤트만 실행할때 사용
   TouchableWithoutFeedback,
   Pressable,
+  TextInput,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./colors";
+let auto;
+const STORAGE_KEY = "@toDos";
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => {
+    console.log(payload)
+    setText(payload);
+  }
+  const saveToDos = async (toSave) => {
+    //로컬스터리지같은개념
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  };
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+  useEffect(() => {
+    loadToDos();
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         {/* 클릭하면 투명되는 애니메이션 */}
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Workaa
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+        //키보드에서 return 버튼
+          returnKeyType="done"
+          //textarea같은거
+          multiline
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          style={styles.input}
+        />
       </View>
     </View>
   );
@@ -45,5 +104,13 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });

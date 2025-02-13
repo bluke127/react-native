77eaad6ee30b2d150eb4a8ce76ef3fe089c67105
bluke127 <dnehdrkd123@naver.com>
2025-02-14 07:21:32 +0900

    [feat]navigator

--- frontend/src/screens/auth/AuthHomeScreen.tsx
+++ frontend/src/screens/auth/AuthHomeScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
 import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
-import {AuthStackParamList} from '../navigation/AuthStackNavigator';
-import {authNaviagtions} from '../constants';
+import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import {authNaviagtions} from '../../constants';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -11,12 +11,15 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    // 스택네비게이터로 설정하면 navigation이 기본 props로
     <SafeAreaView>
       <View>
         <Button
           title="로그인화면으로 이동"
           onPress={() => navigation.navigate(authNaviagtions.LOGIN)}
+        />
+        <Button
+          title="회원가입 이동"
+          onPress={() => navigation.navigate(authNaviagtions.SIGNUP)}
         />
       </View>
     </SafeAreaView>
